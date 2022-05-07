c8e2d2593eb20cec1e0e5549caa329cc41e3dd3c
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-05-07 23:25:57 +0100

    updating UI

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,15 @@
+import LinkForm from "./Link/LinkForm";
 import styles from "./About.module.scss";
 
 const About = function () {
-  return <section className={styles["section-about"]}>&nbsp;</section>;
+  return (
+    <section className={styles["section-about"]}>
+      <LinkForm></LinkForm>
+      <div className='' style={{ marginTop: "50px" }}>
+        &nbsp;
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Link/LinkForm.js
+++ src/components/Link/LinkForm.js
@@ -1,9 +1,21 @@
+import Button from "../UI/Button";
 import styles from "./LinkForm.module.scss";
 
 const LinkForm = function (props) {
   return (
     <form action='/' className={styles.form}>
-      &nbsp;
+      <div className={styles["form__group"]}>
+        <input
+          type='text'
+          className={styles["form__input"]}
+          placeholder='Shorten a link here...'
+          id='link_Input'
+          name='link'
+        />
+      </div>
+      <div className={styles["form__group"]}>
+        <Button className='btn btn--submit'>Shorten It!</Button>
+      </div>
     </form>
   );
 };
