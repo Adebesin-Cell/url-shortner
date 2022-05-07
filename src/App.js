48e3a7f2da8d38f195e47328646b4a945f301f66
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-05-07 17:53:28 +0100

    updating UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "./components/Header";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <Intro></Intro>
     </Fragment>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,34 @@
+import Button from "./UI/Button";
 import styles from "./Intro.module.scss";
+import IntroImage from "../assets/svgs/illustration-working.svg";
 
-const Intro = function () {};
+const Intro = function () {
+  return (
+    <section className={styles["section-intro"]}>
+      <div className={styles.intro}>
+        <div className={styles["intro__content"]}>
+          <h1 className={styles["intro__heading"]}>
+            <span>More than just </span>
+            <span>shorter links</span>
+          </h1>
+          <p className={styles["intro__paragraph"]}>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <Button className='btn btn--intro' type='button' text='Get Started'>
+            Get Started
+          </Button>
+        </div>
+        <div className={styles["intro__image-box"]}>
+          <img
+            src={IntroImage}
+            className={styles["intro__image"]}
+            alt='man working with a desktop'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
