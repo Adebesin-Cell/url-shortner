a2c88f0b1e4f0bdeab2912f1869cad6dbd8b0a4b
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-05-09 21:40:55 +0100

    updating Ui

--- src/components/Link/LinkForm.js
+++ src/components/Link/LinkForm.js
@@ -1,23 +1,64 @@
+import { useRef, useState } from "react";
+import axios from "axios";
 import Button from "../UI/Button";
 import styles from "./LinkForm.module.scss";
 
 // ${styles["form__input--error"]}
 
 const LinkForm = function (props) {
+  const [hasError, setHasError] = useState(false);
+  const inputRef = useRef("");
+  let errorContent;
+
+  const submitFormHandler = async function (e) {
+    e.preventDefault();
+
+    if (inputRef.current.value.trim() === "") {
+      setHasError(true);
+      errorContent = "Please add a link";
+      return;
+    }
+
+    console.log(errorContent);
+
+    console.log(inputRef.current.value);
+
+    try {
+      setHasError(false);
+      const response = await axios.post(
+        `https://api.shrtco.de/v2/shorten?url=${inputRef.current.value}`
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      setHasError(true);
+      errorContent = error.message;
+    }
+
+    inputRef.current.value = "";
+  };
+
   return (
-    <form action='/' className={styles.form}>
-      <div className={styles["form__group"]}>
-        <input
-          type='text'
-          className={`${styles["form__input"]}`}
-          placeholder='Shorten a link here...'
-          id='link_Input'
-          name='link'
-        />
+    <form action='/' className={styles.form} onSubmit={submitFormHandler}>
+      <div className={styles["form__wrapper"]}>
+        <div className={styles["form__group"]}>
+          <input
+            ref={inputRef}
+            type='url'
+            className={`${styles["form__input"]} ${
+              hasError ? styles["form__input--error"] : ""
+            }`}
+            placeholder='Shorten a link here...'
+            id='link_Input'
+            name='link'
+          />
+        </div>
+        <div className={styles["form__group"]}>
+          <Button className='btn btn--submit'>Shorten It!</Button>
+        </div>
       </div>
-      <div className={styles["form__group"]}>
-        <Button className='btn btn--submit'>Shorten It!</Button>
-      </div>
+      {hasError && <p className={styles.error}>Please add a link</p>}
     </form>
   );
 };
--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,7 +1,15 @@
 import styles from "./LinkItem.module.scss";
+import Button from "../UI/Button";
+import { useState } from "react";
 
 const LinkItem = function (props) {
+  const [isCopied, setIsCopied] = useState(false);
   console.log(props);
+
+  const copyLinkHandler = function (copy) {
+    setIsCopied(true);
+  };
+
   return (
     <li className={styles["about__item"]}>
       <p
@@ -9,6 +17,19 @@ const LinkItem = function (props) {
       >
         {props.originalLink}
       </p>
+      <div className={styles["about__extras"]}>
+        <p
+          className={`${styles["about__link"]} ${styles["about__link--shortened"]}`}
+        >
+          {props.shortenedLink}
+        </p>
+        <Button
+          className={`btn btn--copy ${isCopied ? "btn--copied" : ""}`}
+          onClick={copyLinkHandler}
+        >
+          {isCopied ? "Copied!" : " Copy"}
+        </Button>
+      </div>
     </li>
   );
 };
